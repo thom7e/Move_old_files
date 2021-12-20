6d4a8c427035d9bb34b828ebe41f417bd90a5400
thom7e <73536775+thom7e@users.noreply.github.com>
2021-12-20 09:40:39 +0100

    Create old_files.py

---
+++ old_files.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+root_src_dir = "Y:\\1_Einkauf"
+
+counter = 0
+
+### WENN VERSCHOBEN WERDEN SOLL auskommentieren##
+
+#alte_dateien = f"{root_src_dir}\\old_files"
+#if not os.path.exists(alte_dateien):
+    #os.mkdir(alte_dateien)
+
+
+for src_dir, dirs, files in os.walk(root_src_dir):  # os.walkthrough
+    filename = src_dir.split("\\")[-1]
+
+    for file_ in files:
+        date = os.path.getmtime(f"{src_dir}\\{file_}")
+        v = datetime.datetime.fromtimestamp(date)
+        x = v.strftime('%Y\\%m\\%d')
+        if x < "2018":
+            ### WENN VERSCHOBEN WERDEN SOLL auskommentieren##
+            #if os.path.samefile(f"{src_dir}\\{file_}", alte_dateien):
+                #continue
+            #print(f"File \"{file_}\" from {x}")
+            #shutil.copy(f"{src_dir}\\{file_}",alte_dateien)
+
+            ###############################################
+
+            os.remove(f"{src_dir}\\{file_}")
+            counter += 1
+
+    #### LEERE ORDNER LÖSCHEN ########
+    if len(os.listdir(src_dir)) == 0:
+        print(f"{filename} ist leer")
+        os.rmdir(src_dir)
+
+### AUSGABE DER GELÖSCHTEN DATEIEN ############
+print(counter)
+datei = open(f'{root_src_dir}\\anzahl_dateien.txt','w')
+datei.write(f"Insgesamt gelöschte Dateien: {counter}")
+datei.close()
